372bb19ae9a6eef5ebd671aab48e19fb09e75075
Carlos Alberto Filho <carlosalberto_filho@outlook.com>
2022-12-07 22:34:23 -0300

    Route starting

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import {createBrowserRouter, Outlet} from "react-router-dom";
-import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
-} from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
-import {Footer} from "./Components/Footer";
-import {Navbar} from "./Components/NavBar";
-import {Home} from "./Pages/Home";
+import { Footer } from "./Components/Footer";
+import { Navbar } from "./Components/NavBar";
+import { Home } from "./Pages/Home";
 
 function App() {
-
-    const router = createBrowserRouter([
-        {
-            path: "",
-            element: <></>
-        }
-    ])
-    return (
-        <>
-            <Home/>
-        </>
-    );
+  const router = createBrowserRouter([
+    {
+      path: "",
+      element: <></>,
+    },
+  ]);
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
