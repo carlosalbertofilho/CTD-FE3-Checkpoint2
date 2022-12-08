ba10c39efe370a7a6a9554cd32c9f1c593361a29
Gui Parpineli <guiparpineli@GuiPC>
2022-12-07 22:29:16 -0300

    add home component

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,8 +3,8 @@ import {Navbar} from "../../Components/NavBar";
 import {Footer} from "../../Components/Footer";
 
 export function Home() {
-    <>
+    <div className={`main-layout ${theme}`}>
         <Navbar/>
         <Footer/>
-    </>
+    </div>
 }
