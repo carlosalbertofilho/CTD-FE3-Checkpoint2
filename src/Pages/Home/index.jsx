bfffc54a60b36aceeab058bbe0c7a1d42657d64c
carlosalbertofilho <carlosalberto_filho@outlook.com>
2022-12-13 10:09:37 -0300

    fix formatting

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,32 +1,23 @@
-import {useEffect, useState} from "react";
-import {Card} from "../../Components/Card";
+import { useEffect, useState } from "react";
+import { Card } from "../../Components/Card";
 
 export const Home = () => {
-    const [dentist, setDentist] = useState([''])
+  const [dentist, setDentist] = useState([""]);
 
-    useEffect(() => {
-        fetch('https://dhodonto.ctdprojetos.com.br/dentista').then(
-            res =>
-                res.json().then(
-                    data =>
-                        setDentist(data)
-                ))
-    }, []);
-
-    return (
-        <>
-            <h1>Home</h1>
-            <div className="card-grid container">
-                {
-                    dentist.map((a, index) =>
-                        <Card
-                            key={index}
-                            data={a}
-                        />
-                    )
-                }
-            </div>
-        </>
+  useEffect(() => {
+    fetch("https://dhodonto.ctdprojetos.com.br/dentista").then((res) =>
+      res.json().then((data) => setDentist(data))
     );
-};
+  }, []);
 
+  return (
+    <>
+      <h1>Home</h1>
+      <div className="card-grid container">
+        {dentist.map((a, index) => (
+          <Card key={index} data={a} />
+        ))}
+      </div>
+    </>
+  );
+};
